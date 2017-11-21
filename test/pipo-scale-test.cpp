d9c63179c4b31455f826bdb6eb7933037705e688
joseph <joseph.larralde@gmail.com>
2017-11-21 15:22:37 +0100

    updated test to use PiPoTestHost class

--- test/pipo-scale-test.cpp
+++ test/pipo-scale-test.cpp
@@ -9,21 +9,17 @@
 
 #include "PiPoScale.h"
 #include "PiPoTestHost.h"
-#include "PiPoTestReceiver.h"
 
 TEST_CASE ("PiPoScale")
 {
-  // PiPoTestHost host;
-  // host.setGraph("scale");
-
-  PiPoTestReceiver receiver(NULL);
-  PiPoScale scale(NULL); // TODO: there should be a parent
-  scale.setReceiver(&receiver);
+  PiPoTestHost host;
+  host.setGraph("scale");
 
   std::vector<PiPoValue> inputFrame;
+  int check;
 
-  // PiPoStreamAttributes sa;
-  // sa.maxFrames = 100;
+  PiPoStreamAttributes sa;
+  sa.maxFrames = 100;
 
   for(double sampleRate = 100.; sampleRate <= 1000.; sampleRate *= 10.)
   {
@@ -31,13 +27,11 @@ TEST_CASE ("PiPoScale")
     {
       for(unsigned int height = 1; height <= 10; height *= 4)
       {
-        // sa.rate = sampleRate;
-        // sa.dims[0] = width;
-        // sa.dims[1] = height;
+        sa.rate = sampleRate;
+        sa.dims[0] = width;
+        sa.dims[1] = height;
 
-        int check;
-        // check = host.setInputStreamAttributes(sa);
-        check = scale.streamAttributes(false, sampleRate, 0, width, height, NULL, 0, 0, 100);
+        check = host.setInputStreamAttributes(sa);
         REQUIRE (check == 0);
 
         const unsigned int size = width * height;
@@ -51,22 +45,11 @@ TEST_CASE ("PiPoScale")
         {
           WHEN ("Scaling from [1. ; 2. ] to [3. ; 4.]")
           {
-            scale.func.set("lin");
-            scale.inMin.set(0, 1.);
-            scale.inMax.set(0, 2.);
-            scale.outMin.set(0, 3.);
-            scale.outMax.set(0, 4.);
-
-            // host.setAttr("func", "lin");
-            // host.setAttr("scale.inmin", 1.);
-            // host.setAttr("scale.inmax", 2.);
-            // host.setAttr("scale.outmin", 3.);
-            // host.setAttr("scale.outmax", 4.);
-
-            // TODO: no parent: no automatic propagation
-            check = scale.streamAttributes(false, sampleRate, 0, width, height, NULL, 0, 0, 100);
-            // check = host.setInputStreamAttributes(sa); // no need for this with a host
-            REQUIRE (check == 0);
+            host.setAttr("scale.func", "lin");
+            host.setAttr("scale.inmin", 1.);
+            host.setAttr("scale.inmax", 2.);
+            host.setAttr("scale.outmin", 3.);
+            host.setAttr("scale.outmax", 4.);
 
             std::vector<std::vector<PiPoValue> > values = {
               {-1. , 1. },
@@ -76,19 +59,19 @@ TEST_CASE ("PiPoScale")
               { 3. , 5. }
             };
 
-            for(std::size_t v = 0; v < values.size(); ++v)
+            for (std::size_t v = 0; v < values.size(); ++v)
             {
               const PiPoValue inputValue = values[v][0];
               const PiPoValue outputExpected = values[v][1];
               std::fill(inputFrame.begin(), inputFrame.end(), inputValue);
-              check = scale.frames(0., 1., &inputFrame[0], size, 1);
-              // check = host.frames(0., 1., &inputFrame[0], size, 1);
-              REQUIRE (check == 0);
-              // REQUIRE (receiver.values != NULL);
 
-              for(unsigned int sample = 0; sample < size; ++sample)
+              host.reset(); // clear stored received frames
+              check = host.frames(0., 1., &inputFrame[0], size, 1);
+              REQUIRE (check == 0);
+
+              for (unsigned int sample = 0; sample < size; ++sample)
               {
-                CHECK (receiver.values[sample] == Approx(outputExpected) );
+                CHECK (host.receivedFrames[0][sample] == Approx(outputExpected));
               }
             }
 
@@ -96,15 +79,11 @@ TEST_CASE ("PiPoScale")
 
           WHEN ("Scaling from [0. ; 1. ] to [0. ; 127.]")
           {
-            scale.func.set("lin");
-            scale.inMin.set(0, 0.);
-            scale.inMax.set(0, 1.);
-            scale.outMin.set(0, 0.);
-            scale.outMax.set(0, 127.);
-
-            // TODO: no parent: no automatic propagation
-            check = scale.streamAttributes(false, sampleRate, 0, width, height, NULL, 0, 0, 100);
-            REQUIRE (check == 0);
+            host.setAttr("scale.func", "lin");
+            host.setAttr("scale.inmin", 0.);
+            host.setAttr("scale.inmax", 1.);
+            host.setAttr("scale.outmin", 0.);
+            host.setAttr("scale.outmax", 127.);
 
             std::vector<std::vector<PiPoValue> > values = {
               {-1.  , -127. },
@@ -122,29 +101,27 @@ TEST_CASE ("PiPoScale")
               const PiPoValue inputValue = values[v][0];
               const PiPoValue outputExpected = values[v][1];
               std::fill(inputFrame.begin(), inputFrame.end(), inputValue);
-              check = scale.frames(0., 1., &inputFrame[0], size, 1);
+
+              host.reset(); // clear stored received frames
+              check = host.frames(0., 1., &inputFrame[0], size, 1);
               REQUIRE (check == 0);
-              REQUIRE (receiver.values != NULL);
 
               for(unsigned int sample = 0; sample < size; ++sample)
               {
-                CHECK (receiver.values[sample] == Approx(outputExpected) );
+                CHECK (host.receivedFrames[0][sample] == Approx(outputExpected));
               }
             }
           } // Scaling from [0. ; 1. ] to [0. ; 127.]
 
+          //*
           WHEN ("Scaling from [0.5 ; 0.9] to [10. ; 100.] with clipping")
           {
-            scale.func.set("lin");
-            scale.inMin.set(0, 0.5);
-            scale.inMax.set(0, 0.9);
-            scale.outMin.set(0, 10.);
-            scale.outMax.set(0, 100.);
-            scale.clip.set(true);
-
-            // TODO: no parent: no automatic propagation
-            check = scale.streamAttributes(false, sampleRate, 0, width, height, NULL, 0, 0, 100);
-            REQUIRE (check == 0);
+            host.setAttr("scale.func", "lin");
+            host.setAttr("scale.inmin", 0.5);
+            host.setAttr("scale.inmax", 0.9);
+            host.setAttr("scale.outmin", 10.);
+            host.setAttr("scale.outmax", 100.);
+            host.setAttr("scale.clip", true);
 
             std::vector<std::vector<PiPoValue> > values = {
               {-1.  ,  10.  },
@@ -161,18 +138,19 @@ TEST_CASE ("PiPoScale")
               const PiPoValue inputValue = values[v][0];
               const PiPoValue outputExpected = values[v][1];
               std::fill(inputFrame.begin(), inputFrame.end(), inputValue);
-              check = scale.frames(0., 1., &inputFrame[0], size, 1);
+
+              host.reset(); // clear stored received frames
+              check = host.frames(0., 1., &inputFrame[0], size, 1);
               REQUIRE (check == 0);
-              REQUIRE (receiver.values != NULL);
 
               for(unsigned int sample = 0; sample < size; ++sample)
               {
-                CHECK (receiver.values[sample] == Approx(outputExpected) );
+                CHECK (host.receivedFrames[0][sample] == Approx(outputExpected));
               }
             }
 
           }  // Scaling from [0.5 ; 0.9] to [10. ; 100.] with clipping
-
+          //*/
         } // Setup: sample-rate, width, and height
 
       } // height
