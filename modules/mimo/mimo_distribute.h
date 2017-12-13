20ac69a403365da5da1d6c0a6acb7c7fca884f01
Ward Nijman <wardnijman@gmail.com>
2017-12-13 16:34:33 +0100

    Renaming mimo.unispring to mimo.distribute

--- modules/mimo/mimo_distribute.h
+++ modules/mimo/mimo_distribute.h
@@ -2,7 +2,7 @@
  * @file mimo_unispring.h
  * @author Ward Nijman
  *
- * @brief mimo unispring
+ * @brief distribution of points using unispring physical model
  *
  * @copyright
  * Copyright (C) 2016 - 2017 by ISMM IRCAM - Centre Pompidou, Paris, France
@@ -35,9 +35,68 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef mimo_unispring_h
-#define mimo_unispring_h
+#ifndef mimo_distribute_h
+#define mimo_distribute_h
 
+#include "jsoncpp/include/json.h"
+#include "mimo.h"
 
+class unispring_model_data : public mimo_model_data
+{
+private:
+    Json::Value root;
+    Json::Reader reader;
+public:
+    int json_size() override
+    {
+    }
+    char* to_json (char* out, int size) throw() override
+    {
+    }
+    int from_json (const char* json_string) override
+    {
+    }
+};
+
+class MiMoDistribute : public Mimo
+{
+private:
+    int _numbuffers, _numtracks, _bufsize;
+    const PiPoStreamAttributes* _attr;
+public:
+    unispring_model_data model;
+    
+    MiMoDistribute(Parent *parent, Mimo *receiver = nullptr)
+    :   Mimo(parent, receiver)
+    {}
+    
+    ~MiMoDistribute(void)
+    {}
+    
+    int setup (int numbuffers, int numtracks, const int bufsizes[], const PiPoStreamAttributes *streamattr[])
+    {
+        return 0;
+    }
+    
+    int train (int itercount, int trackindex, int numbuffers, const mimo_buffer buffers[])
+    {
+        return 0;
+    }
+    
+    int streamAttributes(bool hasTimeTags, double rate, double offset, unsigned int width, unsigned int height, const char **labels, bool hasVarSize, double domain, unsigned int maxFrames)
+    {
+        return 0;
+    }
+    int frames(double time, double weight, float *values, unsigned int size, unsigned int num)
+    {
+        return 0;
+    }
+    
+    mimo_model_data *getmodel()
+    {
+        return &model;
+    }
+
+};
 
 #endif /* mimo_unispring_h */
