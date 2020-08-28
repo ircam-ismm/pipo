6af9dc71735d8f168bd9cfc408634a1bbb0b2cfc
diemo <diemo.schwarz@ircam.fr>
2020-08-28 15:14:23 +0200

    error reporting for parse error

--- modules/PiPoJs.h
+++ modules/PiPoJs.h
@@ -35,7 +35,16 @@ private:
 
   //jerry_value_t	 input_frame_;	// data frame object to be input to script
   jerry_value_t		 input_array_;   // array object "a" for input data frame when using expr
-
+  std::map<jerry_error_t, const char *> error_name_ = {
+    {JERRY_ERROR_COMMON,    "common error"}, 
+    {JERRY_ERROR_EVAL,	    "eval error"}, 
+    {JERRY_ERROR_RANGE,     "range error"}, 
+    {JERRY_ERROR_REFERENCE, "reference error"}, 
+    {JERRY_ERROR_SYNTAX,    "syntax error"}, 
+    {JERRY_ERROR_TYPE,      "type error"}, 
+    {JERRY_ERROR_URI,       "URI error"} 
+};
+  
 public:
   PiPoScalarAttr<const char *> expr_attr_;
 
@@ -43,58 +52,68 @@ public:
   : PiPo(parent, receiver),
     expr_attr_(this, "expr", "JS expression producing output frame from input in array a", false, "")
   {
-    if (!jerry_initialized)
-    {
-      /* Initialize engine */
-      jerry_init(JERRY_INIT_EMPTY);
-      jerry_initialized = true;
-    }
+    printf("PiPoJs %p ctor\n", this);
 
-    /* Register 'print' function from the extensions to the global object */
-    jerryx_handler_register_global ((const jerry_char_t *) "print", jerryx_handler_print);
-
-    /* Getting pointer to the Global object */
-    global_object_ = jerry_get_global_object ();
-
-    // init js objects to undefined
-    parsed_expr_ = jerry_create_error(JERRY_ERROR_TYPE, (const jerry_char_t *) "no expression");
-    input_array_ = jerry_create_undefined();
-
-    struct {
-      const char *name;
-      jerry_value_t (*handler) (const jerry_value_t function_object, const jerry_value_t function_this,
-				const jerry_value_t arguments[], const jerry_length_t argument_count);
-    } external_functions[] =
-    {
-      { "mtof",  mtof_handler },
-      { "ftom",  ftom_handler },
-      { "atodb", atodb_handler },
-      { "dbtoa", dbtoa_handler }
-    };
-
-    for (int i = 0; i < sizeof(external_functions) / sizeof(external_functions[0]); i++)
-    {
-      // Create a name JS string
-      jerry_value_t property_name  = jerry_create_string((const jerry_char_t *) external_functions[i].name);
-      // Create function from native C method (this function will be called from JS)
-      jerry_value_t property_value = jerry_create_external_function(external_functions[i].handler);
-      // Add the property with the function value to the "global" object
-      jerry_value_t set_result = jerry_set_property(global_object_, property_name, property_value);
-
-      // Check if there was no error when adding the property (in this case it should never happen)
-      if (jerry_value_is_error(set_result)) {
-	throw std::logic_error("Failed to add the function property");
+    try {
+      if (!jerry_initialized)
+      {
+	/* Initialize engine */
+	jerry_init(JERRY_INIT_EMPTY);
+	jerry_initialized = true;
       }
 
-      // Release all jerry_values
-      jerry_release_value (set_result);
-      jerry_release_value (property_value);
-      jerry_release_value (property_name);
+      /* Register 'print' function from the extensions to the global object */
+      jerryx_handler_register_global ((const jerry_char_t *) "print", jerryx_handler_print);
+
+      /* Getting pointer to the Global object */
+      global_object_ = jerry_get_global_object ();
+
+      // init js objects to undefined
+      parsed_expr_ = jerry_create_error(JERRY_ERROR_TYPE, (const jerry_char_t *) "no expression");
+      input_array_ = jerry_create_undefined();
+
+      struct {
+	const char *name;
+	jerry_value_t (*handler) (const jerry_value_t function_object, const jerry_value_t function_this,
+				  const jerry_value_t arguments[], const jerry_length_t argument_count);
+      } external_functions[] =
+	  {
+	    { "mtof",  mtof_handler },
+	    { "ftom",  ftom_handler },
+	    { "atodb", atodb_handler },
+	    { "dbtoa", dbtoa_handler }
+	  };
+
+      for (int i = 0; i < sizeof(external_functions) / sizeof(external_functions[0]); i++)
+      {
+	// Create a name JS string
+	jerry_value_t property_name  = jerry_create_string((const jerry_char_t *) external_functions[i].name);
+	// Create function from native C method (this function will be called from JS)
+	jerry_value_t property_value = jerry_create_external_function(external_functions[i].handler);
+	// Add the property with the function value to the "global" object
+	jerry_value_t set_result = jerry_set_property(global_object_, property_name, property_value);
+
+	// Check if there was no error when adding the property (in this case it should never happen)
+	if (jerry_value_is_error(set_result)) {
+	  throw std::logic_error("Failed to add the function property");
+	}
+
+	// Release all jerry_values
+	jerry_release_value (set_result);
+	jerry_release_value (property_value);
+	jerry_release_value (property_name);
+      }
+    }
+    catch (std::exception &e)
+    {
+      printf("ARGH!!!! PiPoJs ctor caught: %s\n", e.what());
     }
   }
 
   ~PiPoJs (void)
   {
+    printf("PiPoJs %p dtor\n", this);
+
     /* Releasing the Global object */
     jerry_release_value (global_object_);    
     jerry_release_value(parsed_expr_);
@@ -206,6 +225,8 @@ public:
                         const char **labels, bool hasVarSize,
                         double domain, unsigned int maxFrames)
   {
+    printf("PiPoJs %p streamAttributes\n", this, );
+
     try {
       // we need to store the max frame size in case hasVarSize is true
       framesize_ = width * height; 
@@ -218,8 +239,22 @@ public:
         parsed_expr_ = jerry_parse(NULL, 0, (const jerry_char_t *) expr_str, expr_len, JERRY_PARSE_NO_OPTS);
 
 	if (jerry_value_is_error(parsed_expr_))
-	  throw std::logic_error(std::string("can't parse js expression '") + expr_str + "'");
-
+	{
+	  jerry_error_t errtype = jerry_get_error_type(parsed_expr_);
+	  jerry_value_t errval  = jerry_get_value_from_error(parsed_expr_, false);
+	  std::string   errmsg = "(no message)";
+	  
+	  if (jerry_value_is_string(errval))
+	  {
+	    size_t errlen = jerry_get_string_size(errval);
+	    unsigned char errbuf[errlen];
+	    jerry_string_to_char_buffer(errval, errbuf, errlen);
+	    errmsg = (const char *) errbuf;
+	  }
+	  jerry_release_value(errval);
+	  throw std::logic_error(std::string("can't parse js expression '") + expr_str +"': "+ error_name_[errtype] +" '"+ errmsg +"'");
+	}
+	
 	// create js array for pipo input
 	input_array_ = create_array(global_object_, "a", framesize_);
 
