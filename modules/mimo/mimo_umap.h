cf1d695b0b46c12f49a02a6789b6cadea1434030
diemo <diemo.schwarz@ircam.fr>
2021-11-10 11:38:26 +0100

    remove hash function for pair (not applicable for non-templated umap alg), indent

--- modules/mimo/mimo_umap.h
+++ modules/mimo/mimo_umap.h
@@ -3,7 +3,7 @@
  * @file mimo_UMAP.h
  * @author Ward Nijman
  *
- * @brief mimo UMAP using flucoma implementation based on Eigen and Spectral
+ * @brief mimo UMAP using flucoma implementation based on Eigen and Spectra
  *
  * @copyright
  * Copyright (C) 2016 - 2019 by ISMM IRCAM - Centre Pompidou, Paris, France
@@ -47,8 +47,7 @@
 #include <algorithm>
 
 #include "jsoncpp/include/json.h"
-//#include "flucoma-core-src/include/data/FluidDataSet.hpp"
-#include "data/FluidDataSet.hpp"
+#include "data/FluidDataSet.hpp" // includes flucoma-core-src/include/data/FluidDataSet.hpp
 #include "algorithms/public/UMAP.hpp"
 
 
@@ -130,20 +129,6 @@ private:
 };
 
 
-typedef std::array<unsigned, 2> mubu_id; // id of a mubu frame, must be hashable
-
-// specialize template class std::hash<> for mubu_id
-namespace std {
-  template<>
-  class hash<mubu_id> {
-  public:
-    size_t operator()(const mubu_id &p) const
-    { // hash calculation code
-      return static_cast<size_t>(((unsigned long) p[0] << 32) + p[1]);
-    }
-  };
-}
-
 class MimoUMAP: public Mimo
 {
 public:
@@ -158,6 +143,7 @@ public:
         
 public:
   PiPoScalarAttr<PiPo::Enumerate> forward_backward_attr_;
+  PiPoVarSizeAttr<const std::string> columns_attr_;
   PiPoScalarAttr<int>		  num_neighbours_attr_;
   PiPoScalarAttr<int>		  out_dims_attr_;
   PiPoScalarAttr<double>	  min_dist_attr_;
@@ -167,106 +153,106 @@ public:
 
   UMAP_model_data decomposition_;
     
-    MimoUMAP(Parent *parent, Mimo *receiver = nullptr)
-    : Mimo(parent, receiver),
-      forward_backward_attr_(this, "direction", "Mode for decoding: forward or backward", true, fb_),
-      out_dims_attr_        (this, "dims", "Number of Output Dimensions", true, out_dims_),
-      num_neighbours_attr_  (this, "k", "Number of Nearest Neighbours", false, 15),
-      min_dist_attr_	    (this, "mindist", "Minimum Distance", false, 0.1),
-      num_iter_attr_	    (this, "numiter", "Number of Iterations", false, 200),
-      learn_rate_attr_      (this, "learnrate", "Learning Rate", false, 0.1),
-      model_attr_           (this, "model", "The model for processing", true, "")
-    {
-      forward_backward_attr_.addEnumItem("forward",  "Forward transformation from input space to principal component space");
-      forward_backward_attr_.addEnumItem("backward", "Backward transformation from principal component space to input space");
-    }
+  MimoUMAP(Parent *parent, Mimo *receiver = nullptr)
+  : Mimo(parent, receiver),
+    forward_backward_attr_(this, "direction", "Mode for decoding: forward or backward", true, fb_),
+    out_dims_attr_        (this, "dims", "Number of Output Dimensions", true, out_dims_),
+    num_neighbours_attr_  (this, "k", "Number of Nearest Neighbours", false, 15),
+    min_dist_attr_	    (this, "mindist", "Minimum Distance", false, 0.1),
+    num_iter_attr_	    (this, "numiter", "Number of Iterations", false, 200),
+    learn_rate_attr_      (this, "learnrate", "Learning Rate", false, 0.1),
+    model_attr_           (this, "model", "The model for processing", true, "")
+  {
+    forward_backward_attr_.addEnumItem("forward",  "Forward transformation from input space to principal component space");
+    forward_backward_attr_.addEnumItem("backward", "Backward transformation from principal component space to input space");
+  }
     
-    ~MimoUMAP(void)
-    {}
+  ~MimoUMAP(void)
+  {}
     
-    int setup (int numbuffers, int numtracks, const int tracksize[], const PiPoStreamAttributes *streamattr[])
+  int setup (int numbuffers, int numtracks, const int tracksize[], const PiPoStreamAttributes *streamattr[])
+  {
+    attr_       = *streamattr;
+    numbuffers_ = numbuffers;
+    numtracks_  = numtracks;
+    bufsizes_.assign(tracksize, tracksize + numbuffers);
+    m_ = 1; // we treat matrix data as an unrolled vector
+    n_ = streamattr[0]->dims[0] * streamattr[0]->dims[1]; // input dimension
+    int out_dims = 2;
+    numframestotal_ = 0;	// total number of frames over all buffers
+    for (int i = 0; i < numbuffers_; i++)
+      numframestotal_ += bufsizes_[i];
+
+    // set output stream attributes
+    PiPoStreamAttributes** outattr = new PiPoStreamAttributes*[numbuffers_];
+
+    for (int i = 0; i < numbuffers_; ++i)
     {
-      attr_       = *streamattr;
-      numbuffers_ = numbuffers;
-      numtracks_  = numtracks;
-      bufsizes_.assign(tracksize, tracksize + numbuffers);
-      m_ = 1; // we treat matrix data as an unrolled vector
-      n_ = streamattr[0]->dims[0] * streamattr[0]->dims[1]; // input dimension
-      int out_dims = 2;
-      numframestotal_ = 0;	// total number of frames over all buffers
-      for (int i = 0; i < numbuffers_; i++)
-	numframestotal_ += bufsizes_[i];
+      outattr[i] = new PiPoStreamAttributes(**streamattr);
+      outattr[i]->dims[0] = out_dims;
+      outattr[i]->dims[1] = 1;
 
-      // set output stream attributes
-      PiPoStreamAttributes** outattr = new PiPoStreamAttributes*[numbuffers_];
-
-      for (int i = 0; i < numbuffers_; ++i)
-      {
-	outattr[i] = new PiPoStreamAttributes(**streamattr);
-	outattr[i]->dims[0] = out_dims;
-	outattr[i]->dims[1] = 1;
-
-	// create labels
-	outattr[i]->labels = new const char*[out_dims];
-	outattr[i]->numLabels = out_dims;
-	outattr[i]->labels_alloc = out_dims;
+      // create labels
+      outattr[i]->labels = new const char*[out_dims];
+      outattr[i]->numLabels = out_dims;
+      outattr[i]->labels_alloc = out_dims;
 	
-	for (int j = 0; j < out_dims; j++)
-	{
-	  char *lab = (char *) malloc(8); //todo: memleak!
-	  snprintf(lab, 8, "UMAP%d", j);
-	  outattr[i]->labels[j] = lab;
-	}
+      for (int j = 0; j < out_dims; j++)
+      {
+	char *lab = (char *) malloc(8); //todo: memleak!
+	snprintf(lab, 8, "UMAP%d", j);
+	outattr[i]->labels[j] = lab;
       }
-
-      return propagateSetup(numbuffers, numtracks, tracksize, const_cast<const PiPoStreamAttributes**>(outattr));
     }
+
+    return propagateSetup(numbuffers, numtracks, tracksize, const_cast<const PiPoStreamAttributes**>(outattr));
+  }
 
 public:	
-    int train (int itercount, int trackindex, int numbuffers, const mimo_buffer buffers[])
+  int train (int itercount, int trackindex, int numbuffers, const mimo_buffer buffers[])
+  {
+    // convert input data into flucoma dataset
+    //fluid::FluidDataSet<mubu_id, PiPoValue, 1> dataset_in;
+    fluid::FluidDataSet<std::string, double, 1> dataset_in(n_); // todo: set capacity to numframestotal_ rows
+      
+    // not one single contiguous block, go point by point
+    for (int bufferindex = 0; bufferindex < numbuffers; bufferindex++)
     {
-      // convert input data into flucoma dataset
-      //fluid::FluidDataSet<mubu_id, PiPoValue, 1> dataset_in;
-      fluid::FluidDataSet<std::string, double, 1> dataset_in(n_); // todo: set capacity to numframestotal_ rows
-      
-      // not one single contiguous block, go point by point
-      for (int bufferindex = 0; bufferindex < numbuffers; bufferindex++)
+      int numframes = buffers[bufferindex].numframes;
+      bufsizes_[bufferindex] = numframes; // input track size might have changed since setup
+      PiPoValue* bufferptr = buffers[bufferindex].data;
+
+      // append to traindata
+      for (int i = 0; i < numframes; i++, bufferptr += n_)
       {
-	int numframes = buffers[bufferindex].numframes;
-	bufsizes_[bufferindex] = numframes; // input track size might have changed since setup
-	PiPoValue* bufferptr = buffers[bufferindex].data;
-
-	// append to traindata
-	for (int i = 0; i < numframes; i++, bufferptr += n_)
-	{
-          //const mubu_id id{bufferindex, i};
-	  std::string id = std::to_string(((unsigned long) bufferindex << 32) + i); // cram 2 ints into a string, todo: use hex or base64
-	  // convert and copy to umap-needed double data
-	  std::vector<double> vec(n_);
-	  std::copy(bufferptr, bufferptr + n_, vec.begin());
-          dataset_in.add(id, fluid::FluidTensorView<double, 1>(vec.data(), 0, n_));
-	}
+	//const mubu_id id{bufferindex, i};
+	std::string id = std::to_string(((unsigned long) bufferindex << 32) + i); // cram 2 ints into a string, todo: use hex or base64
+	// convert and copy to umap-needed double data
+	std::vector<double> vec(n_);
+	std::copy(bufferptr, bufferptr + n_, vec.begin());
+	dataset_in.add(id, fluid::FluidTensorView<double, 1>(vec.data(), 0, n_));
       }
+    }
 
-      // actually do the UMAP
-      fluid::algorithm::UMAP myUMAP;	      // make a UMAP object
-      const int    k         = std::max<int>(num_neighbours_attr_.get(), 1);
-      const double mindist   = std::max<double>(min_dist_attr_.get(), 0);
-      const int    numiter   = std::max<int>(num_iter_attr_.get(), 1);
-      const double learnrate = std::max<double>(std::min<double>(learn_rate_attr_.get(), 1), 0);
+    // actually do the UMAP
+    fluid::algorithm::UMAP myUMAP;	      // make a UMAP object
+    const int    k         = std::max<int>(num_neighbours_attr_.get(), 1);
+    const double mindist   = std::max<double>(min_dist_attr_.get(), 0);
+    const int    numiter   = std::max<int>(num_iter_attr_.get(), 1);
+    const double learnrate = std::max<double>(std::min<double>(learn_rate_attr_.get(), 1), 0);
       
 
-      //if (k > src.size())
-      //  return Error("Number of Neighbours is larger than dataset");
+    //if (k > src.size())
+    //  return Error("Number of Neighbours is larger than dataset");
 
-      // should be: fluid::FluidDataSet<mubu_id, PiPoValue, 1>, but umap only works on
-      fluid::FluidDataSet<std::string, double, 1> embedding = myUMAP.train(dataset_in, k, out_dims_, mindist, numiter, learnrate);
+    // should be: fluid::FluidDataSet<mubu_id, PiPoValue, 1>, but umap only works on
+    fluid::FluidDataSet<std::string, double, 1> embedding = myUMAP.train(dataset_in, k, out_dims_, mindist, numiter, learnrate);
 
-      if (1) 
-      { // ok
-        // copy back to output track, point by point 
-        fluid::FluidTensorView<double, 2>      out_points = embedding.getData();
-        fluid::FluidTensorView<std::string, 1> out_ids    = embedding.getIds(); //ids should match, but IIRC ordering isn’t guaranteed, so better to grab again
+    if (1) 
+    { // ok
+      // copy back to output track, point by point 
+      fluid::FluidTensorView<double, 2>      out_points = embedding.getData();
+      fluid::FluidTensorView<std::string, 1> out_ids    = embedding.getIds(); //ids should match, but IIRC ordering isn’t guaranteed, so better to grab again
 
       // split and copy transformed input data (embedding) to output buffers
       std::vector<std::vector<PiPoValue>> outdata(numbuffers); // temp space for output data, will be deallocated at end of function
@@ -306,94 +292,94 @@ public:
       }
 #endif
 	
-	return propagateTrain(itercount, trackindex, numbuffers, &outbufs[0]);
-      }
-      else
-      {
-	signalWarning("UMAP Error, propagating empty matrix");
-	std::vector<mimo_buffer> invalidbuf(numbuffers);
-	return propagateTrain(itercount, trackindex, numbuffers, &invalidbuf[0]);
-      }
-    }// end train
-    
-    mimo_model_data *getmodel ()
-    {
-        return &decomposition_;
+      return propagateTrain(itercount, trackindex, numbuffers, &outbufs[0]);
     }
-    
-    int streamAttributes (bool hasTimeTags, double rate, double offset, unsigned int width, unsigned int height, const char **labels, bool hasVarSize, double domain, unsigned int maxFrames)
+    else
     {
-	if(decomposition_.from_json(model_attr_.getJson()) != -1)
-	{
-	}
-	else
-	{
-	    signalWarning("UMAP not configured yet.");
-	}
-        
-        fb_ = forward_backward_attr_.get();
-        
-        unsigned int outn = 0, outm = 0;	// todo: check rank_attr_ if different outn requested
-        
-        switch(fb_)
-        {
-            case Forward:
-            {
-                break;
-            }
-            case Backward:
-            {
-                break;
-            }
-            default:
-            {
-                signalWarning("Mode can either be 'backward' or 'forward'");
-                break;
-            }
-        }
-        return propagateStreamAttributes(hasTimeTags, rate, offset, outn, outm, NULL, 0, 0.0, maxFrames);
-    } // end streamAttributes
+      signalWarning("UMAP Error, propagating empty matrix");
+      std::vector<mimo_buffer> invalidbuf(numbuffers);
+      return propagateTrain(itercount, trackindex, numbuffers, &invalidbuf[0]);
+    }
+  } // end train
     
-    int frames (double time, double weight, float *values, unsigned int size, unsigned int num)
+  mimo_model_data *getmodel ()
+  {
+    return &decomposition_;
+  }
+    
+  int streamAttributes (bool hasTimeTags, double rate, double offset, unsigned int width, unsigned int height, const char **labels, bool hasVarSize, double domain, unsigned int maxFrames)
+  {
+    if(decomposition_.from_json(model_attr_.getJson()) != -1)
     {
-      if (0)
-      { //model not configured, propagate zero matrix
-        return propagateFrames(time, weight, new float[1](), 1, 1);
-      }
-      else
-        switch (fb_)
-        {
-            /*
-	    case Forward:
-            {
-                if ((long) size < n_)
-                {
-                    signalWarning("Vector too short, input should be a vector with length n");
-                    return propagateFrames(time, weight, nullptr, 0, 0);
-                }
+    }
+    else
+    {
+      signalWarning("UMAP not configured yet.");
+    }
+        
+    fb_ = forward_backward_attr_.get();
+        
+    unsigned int outn = 0, outm = 0;	// todo: check rank_attr_ if different outn requested
+        
+    switch(fb_)
+    {
+    case Forward:
+    {
+      break;
+    }
+    case Backward:
+    {
+      break;
+    }
+    default:
+    {
+      signalWarning("Mode can either be 'backward' or 'forward'");
+      break;
+    }
+    }
+    return propagateStreamAttributes(hasTimeTags, rate, offset, outn, outm, NULL, 0, 0.0, maxFrames);
+  } // end streamAttributes
+    
+  int frames (double time, double weight, float *values, unsigned int size, unsigned int num)
+  {
+    if (0)
+    { //model not configured, propagate zero matrix
+      return propagateFrames(time, weight, new float[1](), 1, 1);
+    }
+    else
+      switch (fb_)
+      {
+	/*
+	  case Forward:
+	  {
+	  if ((long) size < n_)
+	  {
+	  signalWarning("Vector too short, input should be a vector with length n");
+	  return propagateFrames(time, weight, nullptr, 0, 0);
+	  }
                 
-                return propagateFrames(time, weight, features.data(), rank_, num);
-            }
+	  return propagateFrames(time, weight, features.data(), rank_, num);
+	  }
 
-	    case Backward:
-            {
-                if ((long) size < rank_)
-                {
-                    signalWarning("Vector too short, input should be a vector with length rank");
-                    return propagateFrames(time, weight, nullptr, 0, 0);
-                }
+	  case Backward:
+	  {
+	  if ((long) size < rank_)
+	  {
+	  signalWarning("Vector too short, input should be a vector with length rank");
+	  return propagateFrames(time, weight, nullptr, 0, 0);
+	  }
                 	
-                return propagateFrames(time, weight, resynthesized.data(), n_, num);
-            }
+	  return propagateFrames(time, weight, resynthesized.data(), n_, num);
+	  }
                 
-            default:
-            {
-                signalWarning("Error... invalid decoding mode selected");
-                return propagateFrames(time, weight, nullptr, 0, 0);
-            }
-             */
-        }
-    } // end frames
+	  default:
+	  {
+	  signalWarning("Error... invalid decoding mode selected");
+	  return propagateFrames(time, weight, nullptr, 0, 0);
+	  }
+	*/
+      }
+  } // end frames
 };
 
 #endif /* MIMO_UMAP_H */
