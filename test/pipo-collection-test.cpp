776e96290b1ea4640885fcec344b058e1f207640
diemo <diemo.schwarz@ircam.fr>
2022-11-03 16:33:43 +0100

    actually check something...

--- test/pipo-collection-test.cpp
+++ test/pipo-collection-test.cpp
@@ -1,3 +1,5 @@
+/* -*- mode: c; c-basic-offset:2 -*- */
+
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
@@ -15,7 +17,7 @@ TEST_CASE ("Test pipo collection")
 {
   PiPoCollection::init();
 
-  WHEN ("Trying to instantiate a pipo chain")
+  WHEN ("Trying to instantiate a pipo graph")
   {
     /*
     PiPo *seg = PiPoCollection::create("slice:fft:sum:scale:onseg");
@@ -25,12 +27,14 @@ TEST_CASE ("Test pipo collection")
     //*
     //PiPo *graph = PiPoCollection::create("slice:fft:sum:scale:onseg");
     //PiPo *graph = PiPoCollection::create("slice:fft<_,sum,moments>");
-    PiPo *graph = PiPoCollection::create("slice<_,fft<sum:scale,moments>>");
+    PiPo *graph = PiPoCollection::create("slice<_,fft<sum:scale,moments>>"); // this graph makes no sense but works
     //PiPo *graph = PiPoCollection::create("slice:fft<sum:scale,moments>");
     //std::shared_ptr<PiPo> graph = std::make_shared<PiPo>(PiPoCollection::create("slice<_,fft<sum:scale,moments>>"));
 
-    if (graph == NULL) return;
     //PiPo *graph = PiPoCollection::create("<sum,moments,_>");
+
+    // Chains / modules are not null
+    REQUIRE (graph != NULL);
     PiPoTestReceiver rx(NULL);
 
     // static_cast<PiPoGraph *>(graph)->setReceiver(&rx);
@@ -39,47 +43,57 @@ TEST_CASE ("Test pipo collection")
 
     // std::cout << graph->getNumAttrs() << std::endl;
 
-    graph->setAttr(0, 10); // slice.size
-    graph->setAttr(1, 5);  // slice.hop
+    const int winsize = 1000;
+    const int hopsize = 100;
+    graph->setAttr(0, winsize); // slice.size
+    graph->setAttr(1, hopsize); // slice.hop
 
     //std::cout << graph->getAttr((unsigned int)0)->getInt(0) << std::endl;
 
-    int ret = graph->streamAttributes(false, 1, 0, 1, 1, NULL, false, 0, 1);
+    int ret = graph->streamAttributes(false, 10000, 0, 1, 1, NULL, false, 0, 1);
 
-    float vals[1024];
-    std::srand(static_cast<unsigned int>(std::time(0)));
-
-    for (unsigned int i = 0; i < 1024; ++i) {
-        vals[i] = std::rand() / static_cast<float>(RAND_MAX);
-    }
-
-    for (unsigned int i = 0; i < 1024; ++i) {
-        graph->frames(0, 0, &vals[i], 1, 1);
-    }
-    //*/
-
-    THEN ("Chains / modules should be instantiated")
+    THEN ("Graph is ok")
     {
-      // Chains / modules are not null
-
-      //sleep(1);
-
-      /*
-      std::cout << ret << std::endl;
-      std::cout << rx.count_error << std::endl;
-      std::cout << rx.sa.rate << std::endl;
-      std::cout << rx.sa.dims[0] << " " << rx.sa.dims[1] << std::endl;
+      CHECK(rx.sa.rate == 100);
+      CHECK(rx.sa.dims[0] == 6);	// 2 unnamed columns from _ and sum:scale, 4 moment
+      CHECK(rx.sa.dims[1] == winsize);	// slice is column vector, so output frame is nonsensical 100 x 6 matrix
 
       for (int i = 0; i < rx.sa.numLabels; i++)
       {
-        std::cout << rx.sa.labels[i] << " ";
+        std::cout << rx.sa.labels[i] << ", ";
       }
       std::cout << std::endl;
+    }
 
-      std::cout << rx.count_frames << std::endl;
+    WHEN ("Sending data")
+    {
+      const int numsamples = 10000;
+      float vals[numsamples];
+      std::srand(static_cast<unsigned int>(std::time(0)));
 
-      //REQUIRE (graph != NULL);
-      //*/
+      for (unsigned int i = 0; i < numsamples; ++i)
+      {
+        vals[i] = std::rand() / static_cast<float>(RAND_MAX);
+      }
+
+      for (unsigned int i = 0; i < numsamples; ++i) {
+        graph->frames(0, 0, &vals[i], 1, 1);
+      }
+
+      THEN ("Received data is ok")
+      {
+	CHECK(rx.count_error == 0);
+	std::cout << rx.count_frames << std::endl;
+
+	// check we get the signal back
+	double absmean = 0;
+	for (int i = 0; i < winsize; i++)
+	  absmean += fabs(rx.values[i * rx.sa.dims[0]]);
+	absmean /= winsize;
+	
+	CHECK(absmean > 0);
+	CHECK(rx.values[2]  > 0); // centroid
+      }
     }
   }
 }
