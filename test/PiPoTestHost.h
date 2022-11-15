81d1d2080f242d26540d0f270e5c303adb849b3f
diemo <diemo.schwarz@ircam.fr>
2022-11-15 12:33:04 +0100

    simplify frame copy, extend PiPoTestHost from testreceiver: count errors

--- test/PiPoTestHost.h
+++ test/PiPoTestHost.h
@@ -1,33 +1,70 @@
 #ifndef _PIPO_TEST_HOST_
 #define _PIPO_TEST_HOST_
 
+#include <float.h>
 #include "PiPoHost.h"
 
 class PiPoTestHost : public PiPoHost
 {
 public:
-    std::vector<std::vector<PiPoValue>> receivedFrames;
+  // capture output frames
+  std::vector<std::vector<PiPoValue>> receivedFrames;
+
+  // capture arguments of last call of frames()
+  // TODO: capture all
+  double  last_time = DBL_MAX;
+  int     last_size = -1;
+  double  end_time  = DBL_MAX;
+
+  // count messages from pipos
+  unsigned int count_error = 0;
+  unsigned int count_warning = 0;
+  unsigned int count_finalize = 0;
+
+  // capture errors
+  std::string last_error;
+  std::string last_warning;
 
 private:
-    // this works when streamAttributesChanged is protected in PiPoHost class
-    // void streamAttributesChanged(PiPo *pipo, PiPo::Attr *attr)
-    // {
-    //     this->propagateInputStreamAttributes();
-    //     std::cout << "just re-propagated stream attributes because an attribute value changed" << std::endl;
-    // }
+  // this works when streamAttributesChanged is protected in PiPoHost class
+  // void streamAttributesChanged(PiPo *pipo, PiPo::Attr *attr)
+  // {
+  //     this->propagateInputStreamAttributes();
+  //     std::cout << "just re-propagated stream attributes because an attribute value changed" << std::endl;
+  // }
 
-    void onNewFrame(double time, double weight, PiPoValue *values, unsigned int size)
-    {
-        std::vector<PiPoValue> frame(size);
+  void onNewFrame(double _time, double _weight, PiPoValue *_values, unsigned int _size) override
+  {
+    last_time   = _time;
+    last_size   = _size;
+    std::vector<PiPoValue> frame(_values, _values + _size); // copy values via input iterator
+    receivedFrames.emplace_back(frame);
+  }
 
-        for (unsigned int i = 0; i < size; ++i)
-        {
-            frame[i] = values[i];
-        }
+  /** called by pipo to signal error in parameters */
+  virtual void signalError(PiPo *pipo, std::string errorMsg) override
+  {
+    count_error++;
+    last_error = errorMsg;
+    printf("error: PiPoTestHost::signalError: %s\n", errorMsg.c_str());
+  }
 
-        receivedFrames.push_back(frame);
-    }
+  /** called by pipo to signal warning in parameters */
+  virtual void signalWarning(PiPo *pipo, std::string errorMsg) override
+  {
+    count_warning++;
+    last_warning = errorMsg;
+    printf("warning: PiPoTestHost::signalWarning: %s\n", errorMsg.c_str());
+  }
 
+  /* TODO: host doesn't handle finalize yet
+  virtual int finalize (double inputEnd) override
+  {
+    count_finalize++;
+    end_time = inputEnd;
+    return 0;
+  }
+*/
 public:
     void reset()
     {
