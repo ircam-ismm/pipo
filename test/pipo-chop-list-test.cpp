709923b1cded95c1993a6f0eb0ffc639be5ab190
diemo <diemo.schwarz@ircam.fr>
2023-06-27 16:33:38 +0200

    add tests for short segments (item 3 from https://git.forum.ircam.fr/haddad/mubu/-/issues/280)

--- test/pipo-chop-list-test.cpp
+++ test/pipo-chop-list-test.cpp
@@ -13,14 +13,18 @@ extern "C" {
 
 TEST_CASE ("chop-list", "[seg]")
 {
-  const int    n_expected = 5;
-  const std::vector<double> t_expected  = { 0, 100, 200, 300, 400 }; // expected chop times
-  const std::vector<double> v_expected1 = { 45, 145, 245, 345, 445 }; // expected chop mean values
-  const std::vector<double> v_expected2 = {  5, 145, 225, 345, 445 }; // expected chop mean values
+  const std::vector<double> t_expected  = {  0, 100, 200, 300, 400 };   // expected chop times
+  const std::vector<double> v_expected  = { 45, 145, 245, 345, 445 };   // expected chop mean values for regular seg.
 
-  const std::vector<double> t_durations0 = { 100, 100, 100, 100, 100 };   // given segdurations
-  const std::vector<double> t_durations1 = { -1, 100, 0, 100, -1 };   // given segdurations
-  const std::vector<double> t_durations2 = { 20, -1, 50, -99, 1000 };   // given segdurations
+  const std::vector<double> t_segtimes   = {   0, 200, 300, 400 };	// given and expected chop times
+  const std::vector<double> t_durations0 = { 200, 100, 100, 100 };	// given segdurations
+  const std::vector<double> t_durations1 = {  -1, 100,   0, -99 };      // given neg. segdurations
+  const std::vector<double> t_durations2 = {  20,  50,  -1, -99 };      // given shorter segdurations
+
+  const std::vector<double> v_expected0  = {  95, 245, 345, 445 };	// expected chop mean values for dur.0/1
+  const std::vector<double> v_expected1  = {  95, 245, 345, 445 };   // expected chop mean values for dur.0/1
+  const std::vector<double> v_expected2  = {   5, 225, 345, 445 };   // expected chop mean values for dur.2
+
 
   int n_samp = 50;
   int t_samp = 500;	// time of last input sample
@@ -39,10 +43,10 @@ TEST_CASE ("chop-list", "[seg]")
   PiPoStreamAttributes sa;
   sa.rate = 100;
 
-  REQUIRE(host.setInputStreamAttributes(sa) == 0);
 
   WHEN ("regular")
   {
+    REQUIRE(host.setInputStreamAttributes(sa) == 0);
     REQUIRE(host.frames(0, 1, &vals[0], 1, n_samp) == 0);
     REQUIRE(host.finalize(t_samp) == 0);
     
@@ -52,11 +56,11 @@ TEST_CASE ("chop-list", "[seg]")
       CHECK(sa.dims[0] == 1);
       CHECK(sa.dims[1] == 1);
 
-      REQUIRE(host.receivedFrames.size() == n_expected);
-      for (unsigned int i = 0; i < n_expected; ++i)
+      REQUIRE(host.receivedFrames.size() == t_expected.size());
+      for (unsigned int i = 0; i < host.receivedFrames.size(); ++i)
       {
 	CHECK(host.received_times_[i]   == t_expected[i]);
-	CHECK(host.receivedFrames[i][0] == v_expected1[i]);
+	CHECK(host.receivedFrames[i][0] == v_expected[i]);
       }
     }
   }
@@ -64,27 +68,74 @@ TEST_CASE ("chop-list", "[seg]")
   WHEN ("with regular segdurations")
   {
     host.reset(); // clear stored received frames
-    host.setAttr("chop.segtimes",     t_expected);
+    host.setAttr("chop.segtimes",     t_segtimes);
     host.setAttr("chop.segdurations", t_durations0);
+    REQUIRE(host.setInputStreamAttributes(sa) == 0);
 
     PiPo::Attr *segt = host.getAttr("chop.segtimes");
     REQUIRE(segt != NULL);
-    CHECK(segt->getSize() == n_expected);
+    CHECK(segt->getSize() == t_segtimes.size());
 
     REQUIRE(host.frames(0, 1, &vals[0], 1, n_samp) == 0);
     REQUIRE(host.finalize(t_samp) == 0);
 
     THEN ("result is ok")
     {
-      PiPoStreamAttributes &sa = host.getOutputStreamAttributes();
-      CHECK(sa.dims[0] == 1);
-      CHECK(sa.dims[1] == 1);
-
-      REQUIRE(host.receivedFrames.size() == n_expected);
-      for (unsigned int i = 0; i < n_expected; ++i)
+      REQUIRE(host.receivedFrames.size() == t_segtimes.size());
+      for (unsigned int i = 0; i < host.receivedFrames.size(); ++i)
       {
-	CHECK(host.received_times_[i]   == t_expected[i]);
+	CHECK(host.received_times_[i]   == t_segtimes[i]);
+	CHECK(host.receivedFrames[i][0] == v_expected0[i]);
+      }
+    }
+  }
+
+  WHEN ("with negative segdurations")
+  {
+    host.reset(); // clear stored received frames
+    host.setAttr("chop.segtimes",     t_segtimes);
+    host.setAttr("chop.segdurations", t_durations1);
+    REQUIRE(host.setInputStreamAttributes(sa) == 0);
+
+    PiPo::Attr *segt = host.getAttr("chop.segtimes");
+    REQUIRE(segt != NULL);
+    CHECK(segt->getSize() == t_segtimes.size());
+
+    REQUIRE(host.frames(0, 1, &vals[0], 1, n_samp) == 0);
+    REQUIRE(host.finalize(t_samp) == 0);
+
+    THEN ("result is ok")
+    {
+      REQUIRE(host.receivedFrames.size() == t_segtimes.size());
+      for (unsigned int i = 0; i < host.receivedFrames.size(); ++i)
+      {
+	CHECK(host.received_times_[i]   == t_segtimes[i]);
 	CHECK(host.receivedFrames[i][0] == v_expected1[i]);
+      }
+    }
+  }
+
+  WHEN ("with shorter segdurations")
+  {
+    host.reset(); // clear stored received frames
+    host.setAttr("chop.segtimes",     t_segtimes);
+    host.setAttr("chop.segdurations", t_durations2);
+    REQUIRE(host.setInputStreamAttributes(sa) == 0);
+
+    PiPo::Attr *segt = host.getAttr("chop.segtimes");
+    REQUIRE(segt != NULL);
+    CHECK(segt->getSize() == t_segtimes.size());
+
+    REQUIRE(host.frames(0, 1, &vals[0], 1, n_samp) == 0);
+    REQUIRE(host.finalize(t_samp) == 0);
+
+    THEN ("result is ok")
+    {
+      REQUIRE(host.receivedFrames.size() == t_segtimes.size());
+      for (unsigned int i = 0; i < host.receivedFrames.size(); ++i)
+      {
+	CHECK(host.received_times_[i]   == t_segtimes[i]);
+	CHECK(host.receivedFrames[i][0] == v_expected2[i]);
       }
     }
   }
