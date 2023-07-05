0d04cd92fa33ad601f5f8107f97930324f40e33c
diemo <diemo.schwarz@ircam.fr>
2023-07-05 11:11:43 +0200

    - fix flux segmentation with startisonset by:   if (keepFirstSegment != 1) // (when in first segment, frameIsOnset is forced to true on every frame, don't set start time)
    - start to integrate segtimes chopping

--- modules/PiPoSegment.h
+++ modules/PiPoSegment.h
@@ -52,7 +52,7 @@ extern "C" {
 #include <string>
 
 
-#define DEBUG_SEGMENT (DEBUG * 1)
+#define DEBUG_SEGMENT (DEBUG * 2)
 
 
 class PiPoSegment : public PiPo
@@ -74,7 +74,9 @@ private:
   bool odfoutput_ = false;
   bool segIsOn = false;
   int keepFirstSegment = 0; // 0: off, 1: wait for first frame (force onset), 2: in first segment
-  
+  std::vector<double> choptimes_;     // cleaned list of chop.at times
+  std::vector<double> chopduration_;  // duration list corresponding to cleaned chop.at times
+
 public:
   PiPoVarSizeAttr<PiPo::Atom> columns_attr_;
   PiPoScalarAttr<int> fltsize_attr_;
@@ -87,7 +89,9 @@ public:
   PiPoScalarAttr<double> maxsegsize_attr_;
   PiPoScalarAttr<bool> odfoutput_attr_;
   PiPoScalarAttr<double> offset_attr_;
-  
+  PiPoVarSizeAttr<double> chopTimesA;
+  PiPoVarSizeAttr<double> chopDurationA;
+
   PiPoSegment (Parent *parent, PiPo *receiver = NULL)
   : PiPo(parent, receiver),
     buffer(), temp(), lastFrame(), 
@@ -101,7 +105,9 @@ public:
     offthresh_attr_   (this, "offthresh",    "Segment End Threshold", false, -80.0),
     maxsegsize_attr_  (this, "maxsize",      "Maximum Segment Duration", false, 0.0),
     odfoutput_attr_   (this, "odfoutput",    "Output only onset detection function", true, false),
-    offset_attr_      (this, "offset",       "Time Offset Added To Onsets [ms]", false, 0)
+    offset_attr_      (this, "offset",       "Time Offset Added To Onsets [ms]", false, 0),
+    chopTimesA(this, "segtimes",  "Fixed Segmentation Times [ms, offset is added], overrides onseg detection", false),
+    chopDurationA(this, "segdurations",  "Fixed Segment Durations [ms], used with chop.segtimes, optional", false)
   {
     onsetmode_attr_.addEnumItem("mean", "Mean");
     onsetmode_attr_.addEnumItem("absmean", "Absolute Mean");
@@ -157,19 +163,29 @@ public:
   
   void reset_segment ()
   {
-    if (this->startisonset_attr_.get())
-    { // start with a segment at 0
-      this->lastFrameWasOnset = true;
-      this->onsetTime = -this->offset;	// first marker will be at 0
-      this->segIsOn = true;
-      this->keepFirstSegment = 1;
+    //settimes(chop.chopTimesA, chop.chopDurationA); // set and clean time/duration lists
+      
+    if (choptimes_.size() == 0)
+    { // use regular chop size
+      if (this->startisonset_attr_.get())
+      { // start with a segment at 0
+	this->lastFrameWasOnset = true;
+	this->onsetTime = -this->offset;	// first marker will be at 0
+	this->segIsOn = true;
+	this->keepFirstSegment = 1;
+      }
+      else
+      {
+	this->lastFrameWasOnset = false;
+	this->onsetTime = -DBL_MAX;
+	this->segIsOn = false;
+	this->keepFirstSegment = 0;
+      }
     }
     else
-    {
-      this->lastFrameWasOnset = false;
-      this->onsetTime = -DBL_MAX;
-      this->segIsOn = false;
-      this->keepFirstSegment = 0;
+    { // use chop times list (is shifted by offset)
+      //last_start_ = choptimes_[0] + offset_; // first segment start
+      //next_time_  = last_start_ + chopduration_[0]; // first segment end
     }
   } // reset_segment
 
@@ -181,7 +197,60 @@ public:
 
     return this->propagateReset();
   };
+
   
+  // set, clean, and normalize chop.at and chop.duration lists:
+  // remove repeating and non-monotonous elements from times, generate normalized durations even when empty
+  void settimes (PiPoVarSizeAttr<double> &times, PiPoVarSizeAttr<double> &durations)
+  {
+    chopduration_.reserve(times.getSize());
+    chopduration_.assign (durations.getPtr(), durations.getPtr() + durations.getSize());
+    choptimes_.assign    (times.getPtr(),     times.getPtr()     + times.getSize());
+      
+    // check and clean times
+    for (size_t i = 0; i < choptimes_.size(); i++)
+    {
+      // clip negative times to 0
+      if (choptimes_[i] < 0)
+	choptimes_[i] = 0;
+        
+      // check strictly monotonous sequence
+      if (i > 0  &&  choptimes_.size() > 1)
+	if (choptimes_[i] <= choptimes_[i - 1])
+	{ // remove times that don't advance (and corresponding duration entries)
+	  choptimes_.erase(choptimes_.begin() + i);
+            
+	  // remove corresponding entry in duration list
+	  if (i < chopduration_.size())
+	    chopduration_.erase(chopduration_.begin() + i);
+	}
+    }
+      
+    // generate normalized durations: clip and fill up to end
+    for (size_t i = 0; i < choptimes_.size(); i++)
+    {
+      // inter-segment-onset time, "inf" end time for last segment (will be clipped to file length)
+      double segduration = (i + 1 < choptimes_.size()  ?  choptimes_[i + 1]  :  DBL_MAX) - choptimes_[i];
+        
+      if (i < chopduration_.size())
+      { // clip duration between 0 and next segment start
+	if (chopduration_[i] <= 0)
+	  chopduration_[i] = segduration;
+	else if (chopduration_[i] > segduration) // avoid overlapping segments (this could be relaxed later)
+	  chopduration_[i] = segduration;
+      }
+      else
+      { // duration list shorter than times list, fill with segment duration
+	chopduration_.push_back(segduration);
+      }
+    }
+      
+#if DEBUG_CHOP
+    for (size_t i = 0; i < choptimes_.size(); i++)
+      printf("%s\t%ld: %6f %6f\n", i == 0 ? "settimes" : "\t", i, NICE_TIME(choptimes_[i]), NICE_TIME(chopduration_[i]));
+#endif
+  } // end settimes()
+    
   template<typename MetricFuncType>
   void calc_onseg_metric (const PiPoValue *values, const unsigned int size, const int filterSize, double &odf, double &energy, MetricFuncType func)
   {
@@ -221,6 +290,7 @@ public:
     double durationThreshold = this->durthresh_attr_.get();
     double offThreshold = this->offthresh_attr_.get();
     enum OnsetMode onset_mode = (enum OnsetMode) this->onsetmode_attr_.get();
+    int ret = 1;
     
     if (size > this->buffer.width)
       size = this->buffer.width; //FIXME: values += size at the end of the loop can be wrong
@@ -229,6 +299,9 @@ public:
     
     for (unsigned int i = 0; i < num; i++)
     { // for all frames
+
+      if (choptimes_.size() == 0)
+      {
       double odf, energy;
       PiPoValue scale = 1.0;
       
@@ -295,7 +368,11 @@ public:
                              &&  time >= this->onsetTime + minimumInterval) // avoid too short inter-onset time
 	                 || keepFirstSegment == 1	     // force immediate first segment be detected
                          || (maxsize > 0  &&  time >= this->onsetTime + maxsize); // when maxsize given, chop unconditionally when segment is longer than maxsize
-      int ret = 1;
+#if DEBUG_SEGMENT > 1
+	  printf("PiPoSegment::frames(%5.1f) ener %6.1f odf %6.1f  onset %d  last %d  seg on %d  keep %d  onset time %6.1f dur %6.1f\n",
+		 time, energy, odf, frameIsOnset, lastFrameWasOnset, segIsOn, keepFirstSegment, 
+		 onsetTime == -DBL_MAX  ?  -1  :  onsetTime, time - (onsetTime == -DBL_MAX  ?  0  :  onsetTime));
+#endif
       
       if (this->odfoutput_)
       { // output odf for each frame
@@ -308,9 +385,9 @@ public:
         bool   frameIsOffset =   energy < offThreshold  // end of segment content
 			     &&  keepFirstSegment == 0;  // override with startisonset: keep silent first segment
 
-        if ((frameIsOnset  // new trigger
-             || (segIsOn  &&  frameIsOffset))  // end of segment content (detect only when we're within a running segment)
-            &&  (duration >= durationThreshold))    // keep only long enough segments //NOT: || !segIsOn (when seg is off, no length condition)
+        if ((frameIsOnset  	               // new trigger
+	     || (segIsOn  &&  frameIsOffset))  // end of segment content (detect only when we're within a running segment)
+            &&  duration >= durationThreshold) // keep only long enough segments //NOT: || !segIsOn (when seg is off, no length condition)
         { // end of segment (new onset or energy below off threshold): propagate segment (on or off)
           // switch off first segment special status
 #if DEBUG_SEGMENT
@@ -329,18 +406,25 @@ public:
         /* segment on/off (segment has at least one frame) */
         if (frameIsOnset)
         {
+	  if (keepFirstSegment != 1) // (when in first segment, frameIsOnset is forced to true on every frame, don't set start time)
+	    this->onsetTime = time; // remember start time of segment
           this->segIsOn = true;
-          this->onsetTime = time;
         }
         else if (frameIsOffset) // offset detected: signal below threshold
           this->segIsOn = false;
+
+	this->lastFrameWasOnset = frameIsOnset;
+	
+	// pass through frames one by one for subsequent temporal modeling modules
+	ret &= this->propagateFrames(time, weight, values, size, 1);
       }
       
-      this->lastFrameWasOnset = frameIsOnset;
-
-      // pass through frames one by one for subsequent temporal modeling modules
-      ret &= this->propagateFrames(time, weight, values, size, 1);
+      }
+      else
+      { // chop segtimes
 	
+      }
+      
       if (ret != 0)
         return ret;
       
@@ -365,7 +449,11 @@ public:
 #endif
       return propagateSegment(this->offset + inputEnd, false);
     }
-    
+#if DEBUG_SEGMENT
+    else
+      printf("PiPoSegment::finalize @ %f  seg on %d  dur %f\n", inputEnd, this->segIsOn, onsetTime == -DBL_MAX  ?  -1  :  duration);
+#endif
+
     return this->propagateFinalize(inputEnd);
   } // finalize
 }; // end class PiPoSegment
