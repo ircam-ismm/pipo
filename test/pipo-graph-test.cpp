55153fcd413e6f40282155a6b324be0cfa1a3cb6
diemo <diemo.schwarz@ircam.fr>
2023-07-18 10:45:40 +0200

    test for 2 parsing bugs with sequence of parallel graphs (<a>b<c> and <a><b>) https://git.forum.ircam.fr/haddad/mubu/-/issues/286

--- test/pipo-graph-test.cpp
+++ test/pipo-graph-test.cpp
@@ -11,17 +11,40 @@ extern "C" {
 #include "catch.hpp"
 #include "PiPoTestReceiver.h"
 #include "PiPoCollection.h"
+
+#define private public /* brutally force access to private parts of graph class */
 #include "PiPoGraph.h"
+#undef private
 
 TEST_CASE ("pipo-graph", "[seg]")
 {
   PiPoCollection::init();
 
-  WHEN ("Instantiate bad syntax graph descr:<chop,gate>")
+  WHEN ("Instantiate complex graph syntax causing parse failure <a><c>")
+  {
+    PiPo *graph = PiPoCollection::create("<thru><thru>"); // another simple graph that should be detected as sequence
+    REQUIRE(graph != NULL);
+    CHECK(static_cast<PiPoGraph *>(graph)->graphtype_ == PiPoGraph::sequence);
+  }
+
+  WHEN ("Instantiate complex graph syntax causing stack overflow on parsing with <a>b<c>")
+  { // https://git.forum.ircam.fr/haddad/mubu/-/issues/286
+    // PiPo *graph = PiPoCollection::create("<yin<select(self):scale(scf),select(selp)>,slice:fft:bands:delta:sum:scale(fsc)>thru<thru,segment:scale>"); // original graph causing stack overflow on parsing
+    PiPo *graph = PiPoCollection::create("<thru>thru<thru>"); // a simple graph that caused stack overflow on parsing
+    REQUIRE(graph != NULL);    // should parse and not crash
+    CHECK(static_cast<PiPoGraph *>(graph)->graphtype_ == PiPoGraph::sequence);
+  }
+
+  WHEN ("Instantiate bad syntax graph descr:<chop,gate> that should return error")
   {
     PiPo *graph = PiPoCollection::create("descr:<chop,gate>"); // a graph with wrong syntax :<
+    // REQUIRE(graph == NULL);    // does not parse!
+    REQUIRE(graph != NULL);    // well, with the above 2 latest fixes, this does parse into the correct graph, so let's accept this syntax for now
+
+    graph = PiPoCollection::create("<thru,thru>:thru"); // a graph with wrong syntax >:
     REQUIRE(graph == NULL);    // does not parse!
-  }
+    // REQUIRE(graph != NULL);    // well, with the above 2 latest fixes, this does parse into the correct graph, so let's accept this syntax for now
+}
 
   const int numsamples = 10000;
   float vals[numsamples];
