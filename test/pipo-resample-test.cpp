c305165f6272acf053421f53f755ab9f211f31c5
Diemo Schwarz <schwarz@ircam.fr>
2025-09-11 10:40:41 +0200

    #537: test targetrate

--- test/pipo-resample-test.cpp
+++ test/pipo-resample-test.cpp
@@ -16,21 +16,23 @@
 TEST_CASE ("resample")
 {
   PiPoTestHost host;
+  PiPoStreamAttributes sa;
+
   host.setGraph("resample");
+  host.setAttr("resample.mode", 1);
+  sa.maxFrames = 32;
 
   int check;
   double insamplerate   = 1000.;
-  double insampleperiod = 1000. / insampleperiod;
+  double insampleperiod = 1000. / insamplerate;
   std::vector<PiPoValue> inputframe;
-  PiPoStreamAttributes sa;
-  int numframes = 10;
-  sa.maxFrames = 20;
+  int numframes = 8;
   
-  for (int timetaggedinput = 0; timetaggedinput <= 1; timetaggedinput += 1)
+  for (int timetaggedinput = 1; timetaggedinput >= 0; timetaggedinput -= 1)
   {
-    for (double factor = 0.125; factor <= 8.; factor *= 2.)
+    for (double factor = 0.25; factor <= 4.; factor *= 2.)
     {
-      for (unsigned int width = 0; width <= 2; width++)
+      for (unsigned int width = 1; width <= 2; width++) //later: check empty frames
       {
 	for (unsigned int height = 1; height <= 3; height *= 3)
 	{
@@ -40,10 +42,6 @@ TEST_CASE ("resample")
 	  sa.hasTimeTags = timetaggedinput;
 	  sa.dims[0] = width;
 	  sa.dims[1] = height;
-	  
-	  check = host.setInputStreamAttributes(sa);
-	  REQUIRE (check == 0);
-
 	  const unsigned int size = width * height;
 	  inputframe.resize(size);
 
@@ -52,14 +50,52 @@ TEST_CASE ("resample")
 				     "factor=" << factor << ", " <<
 				     "width="  << width  << ", " <<
 				     "height=" << height).str();
-	  
+	   
 	  GIVEN (setup)
 	  {
+	    WHEN ("giving targetrate")
+	    {
+	      double time = 0;
+	      double targetrate = insamplerate * factor;
+	      INFO("targetrate " << targetrate);
+	      host.reset(); // clear stored received frames
+	      host.setAttr("resample.targetrate", targetrate);
+	      check = host.setInputStreamAttributes(sa);
+	      REQUIRE (check == 0);
+
+	      for (std::size_t i = 0; i < numframes; i++)
+	      {
+		if (size > 0)
+		{
+		  inputframe[0] = i + 1;
+		  for (int j = 1; j < size; j++)
+		    inputframe[j] = inputframe[j - 1] * (1 + pow(10, i));
+		}
+		
+		check = host.frames(time, 1., inputframe.data(), size, 1);///todo num>1
+		REQUIRE (check == 0);
+
+		time += insampleperiod;
+	      }
+
+	      // check received frames and time tags
+	      double expected_time = insampleperiod; // output is second frame of decimated input group (???)
+	      CHECK (host.receivedFrames.size() == numframes * factor);
+              for (unsigned int i = 0; i < host.receivedFrames.size(); i++)
+              {
+		CHECK (host.received_times_[i] == Approx(expected_time));
+                //CHECK (host.receivedFrames[i][j] == ????);
+		expected_time += outsampleperiod;
+              }
+            } // when
+
 	    WHEN ("giving factor")
 	    {
 	      double time = 0;
 	      host.reset(); // clear stored received frames
 	      host.setAttr("resample.factor", factor);
+	      check = host.setInputStreamAttributes(sa);
+	      REQUIRE (check == 0);
 
 	      for (std::size_t i = 0; i < numframes; i++)
 	      {
@@ -84,7 +120,7 @@ TEST_CASE ("resample")
                 //CHECK (host.receivedFrames[i][j] == ????);
 		expected_time += outsampleperiod;
               }
-            } // when
+            } // when	 
           } // given
 	} // height
       } // width
