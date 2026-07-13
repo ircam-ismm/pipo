a0309cdc209f3d888f68016f99d026fdc695a90b
RiccardoBorghesi <11445033+RiccardoBorghesi@users.noreply.github.com>
2026-07-13 11:04:17 +0200

    finalized PiPo1Euro

--- modules/PiPo1Euro.h
+++ modules/PiPo1Euro.h
@@ -58,8 +58,9 @@ private:
   double frameRate;
   float fcd;
   
-  float flag_2nd, dx_prev1, out_prev, out_prev1, out_prev2, out_prev3;
-  float outVal;
+  std::vector<PiPoValue> dx_prev1, out_prev, out_prev1, out_prev2, out_prev3;
+  //float outVal;
+  float flag_2nd;
 
 public:
   //PiPoScalarAttr<float> fcd;
@@ -78,18 +79,23 @@ public:
   bypass(this, "bypass", "bypass filter (true/false)", true, 0.),
   order(this, "order", "number of consecutive times the filter is applied", true, 1.)
   {
+    this->outValues.resize(1);
+    this->out_prev.resize(1);
+    this->out_prev1.resize(1);
+    this->out_prev2.resize(1);
+    this->out_prev3.resize(1);
+    this->dx_prev1.resize(1);
+    
     this->twopi = M_PI*2;
     this->frameRate = 100.;
     this->fcd = 1.;
     this->flag_2nd = 0;
-    this->dx_prev1 = 0.;
-    this->outValues.resize(1);
     
-    out_prev = 0.;
-    out_prev1 = 0.;
-    out_prev2 = 0.;
-    out_prev3 = 0.;
-    outVal = 0.;
+    this->dx_prev1[0] = 0.;
+    this->out_prev[0] = 0.;
+    this->out_prev1[0] = 0.;
+    this->out_prev2[0] = 0.;
+    this->out_prev3[0] = 0.;
   }
 
   ~PiPo1Euro()
@@ -110,6 +116,13 @@ public:
   int streamAttributes(bool hasTimeTags, double rate, double offset, unsigned int width, unsigned int height, const char **labels, bool hasVarSize, double domain, unsigned int maxFrames)
   {
     this->frameRate = rate;
+    this->outValues.resize(width);
+    this->dx_prev1.resize(width);
+    this->out_prev.resize(width);
+    this->out_prev1.resize(width);
+    this->out_prev2.resize(width);
+    this->out_prev3.resize(width);
+    
     if(this->order.get() > 4) this->order.set(4);
     else if(this->order.get() < 1) this->order.set(1);
     return this->propagateStreamAttributes(hasTimeTags, rate, offset, width, height, labels, false, 0.0, 1);
@@ -119,8 +132,91 @@ public:
   {
     return this->propagateReset();
   }
-
+  
   int frames(double time, double weight, PiPoValue *values, unsigned int size, unsigned int num)
+  {
+    int ret = 0;
+    for(unsigned int i = 0; i < num; i++)
+    {
+      for(unsigned int j = 0; j < size; j++)
+      {
+        float value = values[j];
+        if(this->bypass.get())
+        {
+          out_prev[j] = fixDenorm(value);
+          out_prev1[j] = out_prev[j];
+          out_prev2[j] = out_prev[j];
+          out_prev3[j] = out_prev[j];
+
+          outValues[j] = out_prev[j];
+        }
+        else
+        {
+          if(flag_2nd != 0) //next times
+          {
+            float alpha = getAlpha(this->fcd, frameRate);
+            float dx = (value - out_prev[j]) * frameRate; //rate of change : derivative from input with output
+            float edx = alpha * dx + (1. - alpha) * dx_prev1[j];//filtered rate of change
+            dx_prev1[j] = fixDenorm(edx);
+            float fc = this->fcmin.get() + this->beta.get() * abs(edx);//adaptative cut-off
+            alpha = getAlpha(fc, frameRate);
+            
+            switch(this->order.get())
+            {
+              default:
+              case 1:
+                out_prev[j] = fixDenorm(alpha * value + (1. - alpha) * out_prev[j]);
+                out_prev1[j] = out_prev[j];
+                out_prev2[j] = out_prev[j];
+                out_prev3[j] = out_prev[j];
+                outValues[j] = out_prev[j];
+                break;
+              case 2:
+                out_prev[j] = fixDenorm(alpha * value + (1. - alpha) * out_prev[j]);//filter input
+                out_prev1[j] = fixDenorm(alpha * out_prev[j] + (1. - alpha) * out_prev1[j]);
+                out_prev2[j] = out_prev1[j];
+                out_prev3[j] = out_prev1[j];
+                outValues[j] = out_prev1[j];
+                break;
+              case 3:
+                out_prev[j] = fixDenorm(alpha * value + (1. - alpha) * out_prev[j]);//filter input
+                out_prev1[j] = fixDenorm(alpha * out_prev[j] + (1. - alpha) * out_prev1[j]);
+                out_prev2[j] = fixDenorm(alpha * out_prev1[j] + (1. - alpha) * out_prev2[j]);
+                out_prev3[j] = out_prev2[j];
+                outValues[j] = out_prev2[j];
+                break;
+              case 4:
+                out_prev[j] = fixDenorm(alpha * value + (1. - alpha) * out_prev[j]);//filter input
+                out_prev1[j] = fixDenorm(alpha * out_prev[j] + (1. - alpha) * out_prev1[j]);
+                out_prev2[j] = fixDenorm(alpha * out_prev1[j] + (1. - alpha) * out_prev2[j]);
+                out_prev3[j] = fixDenorm(alpha * out_prev2[j] + (1. - alpha) * out_prev3[j]);
+                outValues[j] = out_prev3[j];
+                break;
+            }
+         }
+          else //the first time in the loop
+          {
+            flag_2nd = 1;
+            out_prev[j] = fixDenorm(value);
+            out_prev1[j] = out_prev[j];
+            out_prev2[j] = out_prev[j];
+            out_prev3[j] = out_prev[j];
+            outValues[j] = out_prev[j];
+          }
+        }
+      }
+      
+      //for(int i = 0; i < size; i++)
+      //this->outValues[i] = outVal[i];
+
+      ret += this->propagateFrames(time, weight, this->outValues.data(), size, 1);
+      
+      values += size;
+    }
+    return ret;
+  }
+
+  /*int frames(double time, double weight, PiPoValue *values, unsigned int size, unsigned int num)
   {
     int ret = 0;
     for(unsigned int i = 0; i < num; i++)
@@ -199,7 +295,7 @@ public:
       values += size;
     }
     return ret;
-  }
+  }*/
 
 };
 
